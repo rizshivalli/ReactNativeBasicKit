538044d62a1df0b2c02ac33d5d5ef398e231cdd4
Rizwan Shivalli <rizwan.a@simplificommerce.com>
2020-02-05 19:45:29 +0530

    react-native-paper added as theme provider

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {Provider} from 'react-redux';
+import {Provider as StoreProvider} from 'react-redux';
+import {Provider as PaperProvider} from 'react-native-paper';
 import NetworkListener from './src/services/NetworkListener';
 
 import configureStore from './src/redux/store/store';
@@ -13,10 +14,12 @@ import configureStore from './src/redux/store/store';
 const store = configureStore();
 
 const RNApp = () => (
-  <Provider store={store}>
-    <App />
-    <NetworkListener />
-  </Provider>
+  <StoreProvider store={store}>
+    <PaperProvider>
+      <App />
+      <NetworkListener />
+    </PaperProvider>
+  </StoreProvider>
 );
 
 AppRegistry.registerComponent(appName, () => RNApp);
